cfc6b525a57ecea45a5386aa32a1151807d7a56f
KaluginaMarina <marin.kalugin@gmail.com>
2018-09-02 23:39:12 +0300

    validation

--- lab1/check.php
+++ lab1/check.php
@@ -5,60 +5,55 @@
 	$y = (int) $_POST['valueY'];
 	$r = (int) $_POST['valueR'];
 	function check($x, $y, $r){
-		if ( 
-			($x >= 0 && $x <= $r && $y <=0 && $y >= -$r/2) ||
-			($x <= 0 && $y <= 0 && $x*$x + $y*$y <= $r) ||
-			(-$x <= $r + $y)) {
-			return true;
-		}
-		else {
-			return false;
-		}
+		return($x >= 0 && $x <= $r && $y <=0 && $y >= -$r/2) ||
+			($x <= 0 && $y <= 0 && $x*$x + $y*$y <= $r*$r) ||
+			($x <= 0 && $y >= 0 && -$x <= $r + $y);
 	}
 	$res = check ($x, $y, $r);
 	$time = microtime(true) - $start;
 ?>
 	
-<table>
+<table border="1px" border-collapse="collapse" >
 	<tr>
 		<td>
-			<h2 class="point">X</h2>
+			<h2 class="ansPoint">X</h2>
 		</td>
 		<td>
-			<h2 class="point">Y</h2>
+			<h2 class="ansPoint">Y</h2>
 		</td>
 		<td>
-			<h2 class="point">R</h2>
+			<h2 class="ansPoint">R</h2>
 		</td>
 		<td>
-			<h2 class="point">Result</h2>
+			<h2 class="ansPoint">Result</h2>
 		</td>
 		<td>
-			<h2 class="point">Time</h2>
+			<h2 class="ansPoint">Time</h2>
 		</td>
 		<td>
-			<h2 class="point">ScriptTime [us]</h2>
+			<h2 class="ansPoint">ScriptTime [us]</h2>
 		</td>
 	</tr>
 	<tr>
 		<td>
-			<h2 class="point"><?php echo $x ?></h2>
+			<h2 class="ansPoint"><?php echo $x ?></h2>
 		</td>
 		<td>
-			<h2 class="point"><?php echo $y ?></h2>
+			<h2 class="ansPoint"><?php echo $y ?></h2>
 		</td>
 		<td>
-			<h2 class="point"><?php echo $r ?></h2>
+			<h2 class="ansPoint"><?php echo $r ?></h2>
 		</td>
 		<td>
-			<h2 class="point"><?php echo $res ? "True" : "False" ?></h2>
+			<h2 class="ansPoint"><?php echo $res ? "True" : "False" ?></h2>
 		</td>
 		<td>
-			<h2 class="point"><?php echo $currentTime ?></h2>
+			<h2 class="ansPoint"><?php echo $currentTime ?></h2>
 		</td>
 		<td>
-			<h2 class="point"><?php echo number_format($time, 10, ".", "")*1000000 ?></h2>
+			<h2 class="ansPoint"><?php echo number_format($time, 10, ".", "")*1000000 ?></h2>
 		</td>
 	</tr>
 	
 </table>
+<br>
--- lab1/js.js
+++ lab1/js.js
@@ -1,5 +1,27 @@
 function checkForm() {
-	return true;
+	var error = true;
+	if (document.querySelector('#selectR').value < 0){
+		document.querySelector('#labelR').textContent = "Что-то не так с R";
+		error = false;
+	}else{
+		document.querySelector('#labelR').textContent = "";
+	}
+	var x = Number(document.querySelector('#text').value);
+	if (isNaN(x) ||	x > 5 || x < -3) {
+		document.querySelector('#labelX').textContent = "Х долден быть числом от -3 до 5";
+		error = false;
+	} else{
+		document.querySelector('#labelX').textContent = "";
+	}
+	
+	return error;
+}
+
+function onlyOne(checkbox) {
+	var checkboxes = document.getElementsByName('valueY');
+	checkboxes.forEach((item) => {
+		if (item !== checkbox) item.checked = false
+	});
 }
 
 function submit(e) {
